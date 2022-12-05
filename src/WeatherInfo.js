63d6f7651188b7fbaa8a6e4d90846b3de7044178
Helirian <omoniieva@gmail.com>
2022-12-05 23:12:59 +0200

    added weather temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,10 +16,14 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} size={52} />
-
-          <span className="temperature">{props.data.temperature}</span>
-          <span className="unit"> C</span>
+          <div className="d-flex">
+            <div>
+              <img src={props.data.icon} alt={props.data.description} />
+            </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
